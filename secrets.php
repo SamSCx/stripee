71e9f12f6d9d00388a7fffb69f874fcffb68719d
Sam Schröder <sam@sam-sc.de>
2023-05-14 12:40:02 +0200

    Setup
    Setup

---
+++ secrets.php
@@ -0,0 +1,6 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+$stripeSecretKey = 'sk_test_51FxP3LJXYlKZrbuFilJsEdWJga4qnrQHGqT4JwS2RyNnpurEEuCbinu7CXDAJeemyDTTZN67UjcWAW8KHWi9Dp7x00f8DtfbZJ';
